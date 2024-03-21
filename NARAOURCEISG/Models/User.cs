cb37569ae1aee84ed241e30913c94f9f18802617
Nestor Adonay Rodriguez Alberto <nestor.adonay.rodriguez.alberto@gmail.com>
2024-03-21 09:21:14 -0600

    Se agregaron Anotations al modelo User

--- NARAOURCEISG/Models/User.cs
+++ NARAOURCEISG/Models/User.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NARAOURCEISG.Models
 {
     public partial class User
     {
         public int Id { get; set; }
+        [Required]
         public string UserName { get; set; } = null!;
+
+        [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = null!;
+        [Required]
         public byte Status { get; set; }
         public byte[]? Image { get; set; }
         public int RoleId { get; set; }
