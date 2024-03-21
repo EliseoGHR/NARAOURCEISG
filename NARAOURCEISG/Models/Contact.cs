23ce5a06eb3f152ce9110dec410be315e61ee119
Oswaldo Chilin <oswaldochilin49@gmail.com>
2024-03-21 15:12:43 -0600

    traduccion de las paginas

--- NARAOURCEISG/Models/Contact.cs
+++ NARAOURCEISG/Models/Contact.cs
@@ -7,12 +7,22 @@ namespace NARAOURCEISG.Models
     public partial class Contact
     {
         public int Id { get; set; }
-        [Display(Name = "Customer")]
+        [Display(Name = "Cliente")]
         public int CustomerId { get; set; }
+
+        [Display (Name = "Primer Nombre")]
         public string FirstName { get; set; } = null!;
+
+        [Display (Name = "Apellido")]
         public string LastName { get; set; } = null!;
+
+        [Display (Name = "Correo")]
         public string? Email { get; set; }
+
+        [Display (Name = "Telefono")]
         public string Phone { get; set; } = null!;
+
+        [Display (Name = "Tipo de Contacto")]
         public string ContactType { get; set; } = null!;
 
         public virtual Customer Customer { get; set; } = null!;
